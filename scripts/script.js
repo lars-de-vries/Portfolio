25779cafb9e1eca961326d0df0c2b6067d6c7333
lars-de-vries <112874252+lars-de-vries@users.noreply.github.com>
2023-11-15 12:34:56 +0100

    hamburger menu added

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
 console.log("Howdy!");
+
+
+
+var menuButton = document.querySelector('header nav button');
+
+menuButton.addEventListener('click', function(){
+    var navigation = document.querySelector('header nav');
+    navigation.classList.toggle("menu-active");
+});
